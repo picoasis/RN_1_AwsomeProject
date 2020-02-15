8b515497a9a3587c0f890ebf7f8752201fd833b9
picoasis <374917085@qq.com>
2020-02-15 15:29:32 +0800

    Hello World

---
+++ App.js
@@ -0,0 +1,27 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React,{Component} from 'react';
+import {
+    Text,
+    View,
+    TextInput
+
+    } from 'react-native';
+
+export default class HelloWorldApp extends Component{
+    render(){
+        return(
+            <View style={{flex:1,justifyContent:'center',alignItems:"center"}}>
+            <Text>Hello World!!!</Text>
+            <TextInput></TextInput>
+            </View>
+
+        )
+    }
+}
